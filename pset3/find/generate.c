a0139762b7eb6f71b9e6d970174b697af06686ed
AlexandreSato <sato_alexandre@hotmail.com>
2020-10-26 22:12:29 -0300

    Generator of duly commented random numbers

--- pset3/find/generate.c
+++ pset3/find/generate.c
@@ -10,6 +10,8 @@
  *
  * where n is number of pseudorandom numbers to print
  * and s is an optional seed
+ *
+ * Aluno: Alexandre Nobuharu Sato em 26/10/2020
  ***************************************************************************/
        
 #include <stdio.h>
@@ -21,23 +23,26 @@
 int
 main(int argc, char *argv[])
 {
-    // TODO: comment me
+    // TODO: Test if more than two or three arguments
     if (argc != 2 && argc != 3)
     {
         printf("Usage: %s n [s]\n", argv[0]);
         return 1;
     }
 
-    // TODO: comment me
+    // TODO: Ensuring that the entry is transformed into an integer
     int n = atoi(argv[1]);
 
-    // TODO: comment me
+    /* TODO: Testing whether the seed of randomness has been reported,
+    *        otherwise it will use time as seed */
+
     if (argc == 3)
         srand((unsigned int) atoi(argv[2]));
     else
         srand((unsigned int) time(NULL));
 
-    // TODO: comment me
+    // TODO: Print on the screen the numbers produced by the rest of the 
+    //      of the division of the rand function by the constant LIMIT
     for (int i = 0; i < n; i++)
         printf("%d\n", rand() % LIMIT);
 

849cf680b2f642e8bdf8b94c04ac0368973b569f
Alexandre Nobuharu Sato <sato_alexandre@hotmail.com>
2020-10-31 18:40:40 -0300

    Sort implemented

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -6,9 +6,9 @@
  *
  * Helper functions for Problem Set 3.
  *
- * Aluno: sato_alexandre@hotmail.com em 30/10/2020
+ * Aluno: sato_alexandre@hotmail.com em 31/10/2020
  ***************************************************************************/
-       
+
 #include <cc50.h>
 #include <stdio.h>
 
@@ -38,17 +38,29 @@ void
 sort(int values[], int n)
 {
     // TODO: implement an O(n^2) sort
-    for ( int i = 0; i < n; i++) { //printing the numbers before sort
+    for ( int i = 0; i < n; i++)  //printing the numbers before sort
+    {
 	printf ("%d\n", values[i]);
     }
 
     //sorting the numbers
-    int SMALLEST = values[0];
-    for (int j = 0; j < (n - 1); j++) { //find the smallest number of the array
-    	if ( values[j] < SMALLEST ) {
-	SMALLEST = values[j];
+    for (int k = 0; k < n; k++)
+    {
+	int SMALLEST = k;
+	for (int j = k; j < n; j++)  //find the smallest number of the array
+        {
+	   if ( values[j] < values[SMALLEST])
+	   SMALLEST = j;
 	}
+        int SWAP = values[k]; values[k] = values[SMALLEST]; values[SMALLEST] = SWAP; //Swaping the numbers
     }
-    printf ("\nSMALLEST: %d\n", SMALLEST);
+
+    printf ("\n");
+
+    for ( int i = 0; i < n; i++) //printing the numbers after sort
+    {
+	printf ("%d\n", values[i]);
+    }
+
     return;
 }

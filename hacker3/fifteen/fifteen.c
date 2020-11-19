dcb8edcead6eb4241b38c90ca0fb88cce56be449
alexandresato <sato_alexandre@hotmail.com>
2020-11-19 11:52:02 -0300

    Scrambling implemented in the game of fifteen

--- hacker3/fifteen/fifteen.c
+++ hacker3/fifteen/fifteen.c
@@ -1,8 +1,8 @@
-/***************************************************************************
+ /***************************************************************************
  * fifteen.c
  *
  * CC50
- * Pset 3
+ * Pset 3 ! HACKER VERSION !  Releases: Randomness and GOD MODE
  *
  * Implements The Game of Fifteen (generalized to d x d).
  *
@@ -13,19 +13,23 @@
  *
  * Note that usleep is obsolete, but it offers more granularity than
  * sleep and is simpler to use than nanosleep; `man usleep` for more.
+ *
+ * Aluno: Alexandre Nobuharu Sato em 18/11/2020 sato_alexandre@hotmail.com
  ***************************************************************************/
- 
+
 #define _XOPEN_SOURCE 500
 
 #include <cc50.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
 
 // constants
 #define DIM_MIN 3
 #define DIM_MAX 9
+#define SHUFFLE 10000
 
 
 // board
@@ -112,8 +116,9 @@ main(int argc, char *argv[])
 void
 clear(void)
 {
-    printf("\033[2J");
-    printf("\033[%d;%dH", 0, 0);
+    printf("\033[2J");//Clear Screen
+    printf("\033[%d;%dH", 15, 0);//Move the draw
+    printf("\033[1;93m");//Change Colors
 }
 
 
@@ -125,7 +130,7 @@ void
 greet(void)
 {
     clear();
-    printf("WELCOME TO THE GAME OF FIFTEEN\n");
+    printf("WELCOME TO THE GAME OF FIFTEEN\nBEM  VINDO  AO  JOGO  DOS  QUINZE\n\n\nby: Alexandre Nobuharu Sato em 18/11/2020");
     usleep(2000000);
 }
 
@@ -138,7 +143,68 @@ greet(void)
 void
 init(void)
 {
-    // TODO
+	 // TODO Ok! in 08/11/2020, imputing tiles on the board
+	int TILE = d*d -1;
+	for (int i=0; i<d; i++)
+	{
+		for (int j=0; j<d; j++)
+		{
+			if (d*d %2 == 0 && i == d -1 && j == d -3)//inverting the 2 with 1 at the end of board
+			{
+				board[i][j]= 1;
+				TILE--;
+			}
+			else if (d*d %2 == 0 && i == d -1 && j == d -2) //inverting the 2 with 1 at the end of board
+			{
+				board[i][j]= 2;
+				TILE--;
+			}
+			else
+			{
+				board[i][j]= TILE;
+				TILE--;
+			}
+		}
+	}
+
+	//Introducing randomness
+	srand( (unsigned int) time(NULL));
+	for (int i = 0, MOVEMENT = 0, I_GAP, J_GAP; i < SHUFFLE; i++ )
+	{
+		for (int i=0; i<d; i++)//Searching GAP postions on the array
+		{
+			for (int j=0; j<d; j++)
+			{
+				if (board[i][j] == 0)
+				{
+					I_GAP = i;
+					J_GAP = j;
+				}
+			}
+		}
+		MOVEMENT = rand() % 4; // 0 = up       1 = right      2 = down      3 = left
+		if (MOVEMENT == 0 && I_GAP != 0)//up
+		{
+			board[I_GAP][J_GAP] = board[I_GAP -1][J_GAP];
+			board[I_GAP -1][J_GAP] = 0;
+		}
+		if (MOVEMENT == 1 && J_GAP != d -1)//right
+		{
+			board[I_GAP][J_GAP] = board[I_GAP][J_GAP +1];
+			board[I_GAP][J_GAP +1] = 0;
+		}
+
+		if (MOVEMENT == 2 && I_GAP != d -1)//down
+		{
+			board[I_GAP][J_GAP] = board[I_GAP +1][J_GAP];
+			board[I_GAP +1][J_GAP] = 0;
+		}
+		if (MOVEMENT == 3 && J_GAP != 0)//left
+		{
+			board[I_GAP][J_GAP] = board[I_GAP][J_GAP -1];
+			board[I_GAP][J_GAP -1] = 0;
+		}
+	}
 }
 
 
@@ -149,7 +215,19 @@ init(void)
 void
 draw(void)
 {
-    // TODO
+	// TODO Ok! in 08/11/2020, printing the board
+	for (int i=0; i<d; i++)
+	{
+		for (int j=0; j<d; j++)
+		{
+			if (board[i][j] == 0)
+				printf ("%5c", 95);//printing "-" character
+			else
+				printf ("%5d", board[i][j]);
+		}
+		printf ("\n\n\n");
+	}
+	printf ("\n\n");
 }
 
 
@@ -161,19 +239,61 @@ draw(void)
 bool
 move(int tile)
 {
-    // TODO
-    return false;
+    // TODO Ok! in 09/11/2020
+	int I_TALE_POSITION, J_TALE_POSITION; //Index positions of the tile on the array
+	int I_GAP, J_GAP; //Index positions of the Gap    char "_"95 << int'0'
+	for (int i=0; i<d; i++)//Searching TILE and GAP postions on the array
+	{
+		for (int j=0; j<d; j++)
+		{
+			if (board[i][j] == tile)
+			{
+				I_TALE_POSITION = i;
+				J_TALE_POSITION = j;
+			}
+			if (board[i][j] == 0)
+			{
+				I_GAP = i;
+				J_GAP = j;
+			}
+		}
+	}
+
+	if ((abs(I_TALE_POSITION-I_GAP)==0&&abs(J_TALE_POSITION-J_GAP)==1) || (abs(I_TALE_POSITION-I_GAP)==1&&abs(J_TALE_POSITION-J_GAP)==0))
+	{
+		board[I_GAP][J_GAP] = board[I_TALE_POSITION][J_TALE_POSITION];
+		board[I_TALE_POSITION][J_TALE_POSITION] = 0;
+		return true;
+	}
+	else
+		return false;
 }
 
-
 /*
- * Returns true if game is won (i.e., board is in winning configuration), 
+ * Returns true if game is won (i.e., board is in winning configuration),
  * else false.
  */
 
 bool
 won(void)
 {
-    // TODO
-    return false;
+    // TODO Ok!! in 09/11/2020
+	int TEST = 1;
+	for (int i=0; i<d; i++)
+	{
+		for (int j=0; j<d; j++)
+		{
+			if (i==d-1 && j==d-1)
+				TEST = 0; //last position
+			if (board[i][j] != TEST)
+			{
+			return false;
+			break;
+			}
+			TEST++;
+		}
+	}
+
+
+    return true;
 }
